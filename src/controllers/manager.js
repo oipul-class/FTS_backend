6ce930757ea662d0acb461c5eaa1eb70815d2030
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-04-06 17:48:52 -0300

    controller de gerente

--- src/controllers/manager.js
+++ src/controllers/manager.js
@@ -1,80 +1,130 @@
 const Manager = require("../models/Manager");
 const Branch = require("../models/Branch");
+const Role = require("../models/Role");
 
 module.exports = {
   async index(req, res) {
-    const managers = await Manager.findAll({
-      attributes: ["manager_name", "rg", "cpf", "branch_id"],
-    });
+    try {
+      const managers = await Manager.findAll({
+        attributes: ["manager_name", "rg", "cpf"],
+        include: [
+          {
+            association: "Branch",
+            attributes: ["branch_name", "cep", "branch_email", "place_number"],
+          },
+          {
+            association: "Role",
+            attributes: ["role_name"],
+          },
+        ],
+      });
 
-    res.send(managers);
+      res.send(managers);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
   },
 
   async store(req, res) {
-    const { manager_name, rg, cpf, manager_password, branch_id } = req.body;
-
-    const branch = await Branch.findByPk(branch_id);
-
-    if (!branch) return res.status(404).send({ erro: "afilial não existe" });
-
-    await Manager.create({
+    const {
       manager_name,
       rg,
       cpf,
       manager_password,
       branch_id,
-    });
+      role_id,
+    } = req.body;
 
-    res.status(201).send({ manager_name, rg, cpf, afilial: company });
+    try {
+      const branch = await Branch.findByPk(branch_id);
+      const role = await Role.findByPk(role_id);
+
+      if (!branch || !role)
+        return res.status(404).send({ erro: "afilial ou cargo não existe" });
+
+      const manager = await Manager.create({
+        manager_name,
+        rg,
+        cpf,
+        manager_password,
+        branch_id,
+        role_id,
+      });
+
+      res.status(201).send({
+        manager_name,
+        rg,
+        cpf,
+        branch: branch.branch_name,
+        role: role.role_name,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
   },
 
   async update(req, res) {
     const { id } = req.params;
 
-    const { manager_name, rg, cpf, manager_password, branch_id } = req.body;
+    const {
+      manager_name,
+      rg,
+      cpf,
+      manager_password,
+      branch_id,
+      role_id,
+    } = req.body;
 
-    const manager = await Manager.findByPk(id);
+    try {
+      const manager = await Manager.findByPk(id);
 
-    if (!manager) return res.status(404).send({ erro: "gerente não existe"});
+      if (!manager) return res.status(404).send({ erro: "gerente não existe" });
 
-    if (manager_name) branch.manager_name = manager_name;
-    if (rg) branch.rg = rg;
-    if (cpf) branch.cpf = cpf;
-    if (manager_password) branch.manager_password = manager_password;
+      if (manager_name) manager.manager_name = manager_name;
+      if (rg) manager.rg = rg;
+      if (cpf) manager.cpf = cpf;
+      if (manager_password) manager.manager_password = manager_password;
+      if (branch_id) {
+        const branch = await Branch.findByPk(branch_id);
 
-    if (branch_id) {
-      const branch = await Branch.findByPk(branch_id);
+        if (!branch)
+          return res.status(404).send({ erro: "afilial não existe" });
 
-      if (!branch) return res.status(404).send({ erro: "afilial não existe" });
-      
-      branch.branch_id = branch_id;
+        manager.branch_id = branch_id;
+      }
+      if (role_id) {
+        const role = await Branch.findByPk(role_id);
+
+        if (!role) return res.status(404).send({ erro: "cargo não existe" });
+
+        manager.role_id = role_id;
+      }
+
+      await manager.save();
+
+      res.send(manager);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
     }
-
-    await branch.save();
-
-    res.send(branch);
   },
 
   async delete(req, res) {
     const { id } = req.params;
 
-    const branch = await Branch.findByPk(id, {
-      attributes: [
-        "branch_name",
-        "cep",
-        "branch_email",
-        "place_number",
-        "company_id",
-      ],
-    });
+    const manager = await Manager.findByPk(id);
 
-    if (!branch) return res.status(404).send({ erro: "afilial não existe" });
+    if (!manager) return res.status(404).send({ erro: "gerente não existe" });
 
-    await branch.destroy();
+    await manager.destroy();
 
-    res.send({
-      status: "deletado",
-      afilial: branch,
-    });
+    res.send(
+      {
+        status: "deletado",
+        gerente: manager
+      }
+    )
   },
 };
