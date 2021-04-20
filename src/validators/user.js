d5745f954209cfcc4cef365247836c4589fb7cbb
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-04-20 15:25:16 -0300

    pequena modificação

--- src/validators/user.js
+++ src/validators/user.js
@@ -3,21 +3,23 @@ const { celebrate, Joi, Segments } = require("celebrate");
 module.exports = {
   create: celebrate({
     [Segments.BODY]: Joi.object().keys({
-      user_name: Joi.string().min(5).required(),
       rg: Joi.string().length(12).required(),
       cpf: Joi.string().length(14).required(),
+      user_name: Joi.string().min(5).required(),
       user_password: Joi.string().min(8).required(),
-      manager_id: Joi.number().integer().min(1).required(),
+      user_access: Joi.number().integer().required(),
+      branch_id: Joi.number().integer().min(1).required(),
     }),
   }),
 
   update: celebrate({
     [Segments.BODY]: Joi.object().keys({
-      user_name: Joi.string().min(5),
       rg: Joi.string().length(12),
       cpf: Joi.string().length(14),
+      user_name: Joi.string().min(5),
       user_password: Joi.string().min(8),
-      manager_id: Joi.number().integer().min(1),
+      user_access: Joi.number().integer(),
+      branch_id: Joi.number().integer().min(1),
     }),
   }),
 }
