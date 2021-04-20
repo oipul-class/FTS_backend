1a0e75a3ff9ed40d76b3f434462b2d8e35c7222a
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-04-20 15:22:25 -0300

    uso da função de erros do celebrate

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const routes = require("./routes");
+const { errors } = require("celebrate")
 
 require("./database");
 
@@ -13,5 +14,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+app.use(errors());
 
 module.exports = app;
