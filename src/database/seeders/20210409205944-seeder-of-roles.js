a4ce551a4f6c186586ac2942d9fa1010786e3fb4
pedroHen14 <pedrohenrique_silvasantos@yahoo.com>
2021-04-26 19:29:46 -0300

    Adicionando dados na seeder de cargos

--- src/database/seeders/20210409205944-seeder-of-roles.js
+++ src/database/seeders/20210409205944-seeder-of-roles.js
@@ -4,17 +4,47 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert("roles", [
       {
-        role_name: "compras",
+        role_name: "Gerente de vendas",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
-        role_name: "vendas",
+        role_name: "Gerente de compras",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
-        role_name: "produtos",
+        role_name: "Gerente financeiro",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        role_name: "Gerente de estoque",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        role_name: "Gerente geral",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        role_name: "Vendedor",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        role_name: "Funcionário de estoque",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        role_name: "Funcionário de compras",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        role_name: "Funcionário do financeiro",
         created_at: new Date(),
         updated_at: new Date(),
       },
