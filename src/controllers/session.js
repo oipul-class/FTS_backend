6f9fd03e0830987fb959db5c5f4b8072e9f70732
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-04-26 16:03:14 -0300

    pequena modificação

--- src/controllers/session.js
+++ src/controllers/session.js
@@ -33,7 +33,7 @@ module.exports = {
               model: Role,
             },
             {
-              association: "user_permission"
+              association: "Permissions"
             }
           ]
         });
