53530a428d5450393158013818eac8f6d71cbf9a
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-04-26 16:03:50 -0300

    redirando as associações antigas

--- src/database/index.js
+++ src/database/index.js
@@ -5,30 +5,24 @@ const connection = new Sequelize(DBConfig);
 
 const Branch = require("../models/Branch");
 const Company = require("../models/Company");
-const Manager = require("../models/Manager");
 const Plan = require("../models/Plan");
 const Role = require("../models/Role");
 const User = require("../models/User");
 const Permission = require("../models/Permission");
 const Screen = require("../models/Screen");
-const Employer = require("../models/Employer");
 
 Branch.init(connection);
 Company.init(connection);
-Manager.init(connection);
 Plan.init(connection);
 Role.init(connection);
 User.init(connection);
 Permission.init(connection);
 Screen.init(connection);
-Employer.init(connection);
 
 Branch.associate(connection.models);
 Company.associate(connection.models);
-Manager.associate(connection.models);
 Plan.associate(connection.models);
 Role.associate(connection.models);
 User.associate(connection.models);
 Permission.associate(connection.models);
 Screen.associate(connection.models);
-Employer.associate(connection.models);
