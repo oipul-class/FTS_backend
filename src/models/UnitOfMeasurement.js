26780738a8976efc730d38660df5bb0a93e56e7a
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-05-05 15:43:39 -0300

    model de unidade de medida

---
+++ src/models/UnitOfMeasurement.js
@@ -0,0 +1,22 @@
+import { Model, DataTypes } from 'sequelize';
+
+class UnitOfMeasurement extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        unit_name: DataTypes.STRING(10),
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.hasMany(models.Product);
+  }
+}
+
+module.exports = UnitOfMeasurement;
