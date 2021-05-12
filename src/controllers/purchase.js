fc8e79d838586c57bcbea83840ff31a691ea5e75
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-05-12 15:03:44 -0300

    controller de compra

---
+++ src/controllers/purchase.js
@@ -0,0 +1,83 @@
+const Purchase = require("../models/Purchase");
+const { update } = require("../models/Purchase");
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const purchases = await Purchase.findAll();
+
+      res.send(purchases);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
+  async find(req, res) {
+    try {
+      const { id } = req.params;
+
+      const purchase = await Purchase.findByPk(id);
+
+      res.send(purchase);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
+  async store(req, res) {
+    try {
+      const { payment_method_id, logbook_invetory_id } = req.body;
+
+      const purchase = await Purchase.create({
+        payment_method_id,
+        logbook_invetory_id,
+      });
+
+      res.status(404).send(purchase);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const { payment_method_id } = req.body;
+
+      const purchase = await Purchase.findByPk(id);
+
+      if (!purchase)
+        return res.status(404).send({ erro: "compra não encontrada" });
+
+      if (payment_method_id) purchase.payment_method_id = payment_method_id;
+
+      await purchase.save();
+
+      res.send(purchase);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      const purchase = await Purchase.findByPk(id);
+
+      if (!purchase)
+        return res.status(404).send({ erro: "compra não encontrada" });
+
+      await purchase.destroy();
+
+      res.send();
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+};
