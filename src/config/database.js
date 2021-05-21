e212326dd571ae9d98f3c80a7c46fef067cdbf34
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-05-21 14:32:47 -0300

    database de teste

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   host: "localhost",
   username: "root",
   password: "bcd127",
-  database: "db_fts",
+  database: process.env.NODE_ENV === "test" ? "test_db_fts" : "db_fts",
   dialect: "mysql",
   define: {
     timestamp: true,
