f3a10bc322691e5d2a4b9a7584df1f6242db74ed
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-06-04 16:19:13 -0300

    alteração no controller

--- src/controllers/screen.js
+++ src/controllers/screen.js
@@ -1,28 +1,43 @@
+const { Op } = require("sequelize");
 const Screen = require("../models/Screen");
 
 module.exports = {
   async index(req, res) {
     try {
-      const screens = await Screen.findAll({
-        attributes: ["id", "screen_name", "route"],
-      });
+      const { screen_name } = req.body;
+
+      let screens;
+
+      if (screen_name)
+        screens = await Screen.findAll({
+          attributes: ["id", "screen_name", "route"],
+          where: {
+            screen_name: { [Op.substring]: screen_name },
+          },
+        });
+      else
+        screens = await Screen.findAll({
+          attributes: ["id", "screen_name", "route"],
+        });
 
       res.send(screens);
     } catch (error) {
-      res.status(500).send({ error: error });
+      console.error(error);
+      res.status(500).send(error);
     }
   },
 
   async find(req, res) {
-    const sreenId = req.params.id;
-
     try {
-      const screen = await Screen.findByPk(sreenId, {
+      const { id } = req.params;
+
+      const screen = await Screen.findByPk(id, {
         attributes: ["id", "screen_name", "route"],
       });
 
       res.send(screen);
     } catch (error) {
+      console.error(error);
       res.status(500).send(error);
     }
   },
