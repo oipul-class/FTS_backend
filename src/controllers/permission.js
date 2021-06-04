2b197f538ce19ef116e524df928ac0f362917355
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-06-04 16:45:50 -0300

    modificação nas funções index e find de permissão

--- src/controllers/permission.js
+++ src/controllers/permission.js
@@ -1,9 +1,45 @@
+const { Op } = require("sequelize");
 const Permission = require("../models/Permission");
 
 module.exports = {
   async index(req, res) {
     try {
-      const response = await Permission.findAll({
+      const { permission_name } = req.body;
+
+      let permissions;
+
+      if (permission_name)
+        permissions = await Permission.findAll({
+          attributes: ["id", "permission_name"],
+          where: {
+            permission_name: { [Op.substring]: permission_name },
+          },
+          include: {
+            association: "Screens",
+            attributes: ["id", "screen_name"],
+          },
+        });
+      else
+        permissions = await Permission.findAll({
+          attributes: ["id", "permission_name"],
+          include: {
+            association: "Screens",
+            attributes: ["id", "screen_name"],
+          },
+        });
+
+      res.send(permissions);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
+  async find(req, res) {
+    try {
+      const { id } = req.params;
+
+      const screen = await Permission.findByPk(id, {
         attributes: ["id", "permission_name"],
         include: {
           association: "Screens",
@@ -11,22 +47,9 @@ module.exports = {
         },
       });
 
-      res.send(response);
+      res.send(screen);
     } catch (error) {
-      res.status(500).send(error);
-    }
-  },
-
-  async find(req, res) {
-    const permissionId = req.params.id;
-
-    try {
-      const response = await Permission.findByPk(permissionId, {
-        attributes: ["id", "permission_name"],
-      });
-
-      res.send(response);
-    } catch (error) {
+      console.error(error);
       res.status(500).send(error);
     }
   },
