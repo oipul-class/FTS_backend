31b37fbbed89dbf8e7ab4f43f2960de7ae001425
pedroHen14 <pedrohenrique_silvasantos@yahoo.com>
2021-06-09 23:17:04 -0300

    Adicionando mais planos na seeder

--- src/database/seeders/20210608172736-adding-plans.js
+++ src/database/seeders/20210608172736-adding-plans.js
@@ -2,17 +2,39 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("plans", [
-      {
-        plan_name: "Basico",
-        branch_limit: 2,
-        user_limit_per_branch: 10,
-        use_phone_for_sale: false,
-        value: 50.00,
-        created_at: new Date(),
-        updated_at: new Date(),
-      }
-    ], null);
+    await queryInterface.bulkInsert(
+      "plans",
+      [
+        {
+          plan_name: "Basico",
+          branch_limit: 2,
+          user_limit_per_branch: 10,
+          use_phone_for_sale: false,
+          value: 50.0,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          plan_name: "Intermediário",
+          branch_limit: 5,
+          user_limit_per_branch: 50,
+          use_phone_for_sale: true,
+          value: 200.0,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          plan_name: "Premium",
+          branch_limit: 10,
+          user_limit_per_branch: 100,
+          use_phone_for_sale: true,
+          value: 500.0,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      null
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
