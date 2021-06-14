5a88a6fe5071793e74f9171193021e14caed469d
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-06-14 15:21:14 -0300

    verificação de segurança de usuario

--- src/middleware/SecurityCheck.js
+++ src/middleware/SecurityCheck.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 const auth = require("../config/auth");
 const Branch = require("../models/Branch");
 const Company = require("../models/Company");
+const User = require("../models/User");
+const purchase = require("../controllers/purchase");
 
 module.exports = {
   CompanyUpdateCheck: async (req, res, next) => {
@@ -81,7 +83,43 @@ module.exports = {
         },
       });
 
-      if (!branch) return res.status(400).send({ error: "Filial não existe ou não pertence a companhia logada"})
+      if (!branch)
+        return res.status(400).send({
+          error: "Filial não existe ou não pertence a companhia logada",
+        });
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
+  userUpdateCheck: async (req, res, next) => {
+    try {
+      const token = req.headers.authorization;
+      const [Bearer, retriviedToken] = token.split(" ");
+
+      const { id } = req.params;
+
+      const payload = jwt.verify(retriviedToken, auth.secret);
+
+
+      if (!payload.user_cpf && !payload.user_rg)
+        return res
+          .status(400)
+          .send({ error: "Usuario logado não é um funcionario" });
+
+      const userByPayload = await User.findByPk(payload.id);
+
+      const userByParams = await User.findByPk(id);
+
+      if (!userByParams || !userByPayload) return res.status(404).send({error: "Usuario logado ou requesitado não existe"})
+
+      if (userByPayload.id != userByParams.id)
+        return res
+          .status(400)
+          .send({ error: "Usiario requesitado não é o mesmo que esta logado" });
 
       next();
     } catch (error) {
