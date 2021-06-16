e02a20654a283c56e814fca0ec226645d8936455
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-06-16 15:06:08 -0300

    alterando uma mensagem de erro ná verificação da existencia do produto e criando função de segurança em rotas de atualizar ou deletar um item do inventario de uma filial

--- src/middleware/logbookSecurityFunctions.js
+++ src/middleware/logbookSecurityFunctions.js
@@ -76,9 +76,76 @@ module.exports = {
       if (!product)
         return res.status(400).send({
           error:
-            "Produto requesitado para ser cadastrado no inventario da companhia não existe ou não pertence não esta cadastrado na companhia da filial",
+            "Produto requesitado não existe ou não pertence a companhia",
         });
 
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
+  logbookUpdateCheck: async (req, res, next) => {
+    try {
+      const token = req.headers.authorization;
+      const [Bearer, retriviedToken] = token.split(" ");
+
+      const payload = jwt.verify(retriviedToken, auth.secret);
+
+      if (!payload.user_cpf || !payload.user_rg)
+        return res
+          .status(400)
+          .send({ error: "Usuário logado não é um funcionario" });
+
+      const user = await User.findOne({
+        where: {
+          id: payload.id,
+        },
+        include: [
+          {
+            model: Permission,
+            required: true,
+            where: {
+              permission_name: { [Op.substring]: "Estoque" },
+            },
+          },
+          {
+            model: Branch,
+            attributes: ["id"],
+            include: {
+              model: Company,
+              attributes: ["id"],
+            },
+          },
+        ],
+      });
+
+      if (!user)
+        return res.status(400).send({
+          error: "Usuário não é permitido criar item no inventario da filial",
+        });
+
+      const { id } = req.params;
+
+      const product = await Product.findByPk(id, {
+        attributes: ["id"],
+        include: {
+          model: Company,
+          attributes: ["id"],
+        },
+      });
+
+      if (!product)
+        return res
+          .status(404)
+          .send({ error: "Produto requesitado não existe ou não pertence a companhia" });
+
+      if (!product.Company.id != user.Branch.Company.id)
+        return res.status(400).send({
+          error:
+            "Produto requesitado não pertence a companhia dona da filial que o usuário logado trabalha",
+        });
       next();
     } catch (error) {
       console.error(error);
