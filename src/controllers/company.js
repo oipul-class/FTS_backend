4d9e641c9a34f02387f72fd5e719d4c0c9d33e63
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-06-18 16:59:09 -0300

    alterações para acomodar o endereço em companhia e filial

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -1,6 +1,7 @@
 const { Op } = require("sequelize");
 const Company = require("../models/Company");
 const Plan = require("../models/Plan");
+const Address = require("../models/Address");
 const bcryptjs = require("bcryptjs");
 
 module.exports = {
@@ -13,14 +14,34 @@ module.exports = {
           "fantasy_name",
           "social_reason",
           "place_number",
-          "cep",
           "state",
           "nature_of_the_business",
           "commercial_email",
         ],
-        include: {
-          model: Plan,
-        },
+        include: [
+          {
+            model: Plan,
+            attributes: [
+              "id",
+              "plan_name",
+              "branch_limit",
+              "user_limit_per_branch",
+              "use_phone_for_sale",
+            ],
+          },
+          {
+            model: Address,
+            attributes: [
+              "id",
+              "cep",
+              "street",
+              "complement",
+              "district",
+              "city",
+              "uf",
+            ],
+          },
+        ],
       });
 
       res.send(companies);
@@ -41,15 +62,35 @@ module.exports = {
           "fantasy_name",
           "social_reason",
           "place_number",
-          "cep",
           "state",
           "nature_of_the_business",
           "commercial_email",
           "plan_id",
         ],
-        include: {
-          model: Plan,
-        },
+        include: [
+          {
+            model: Plan,
+            attributes: [
+              "id",
+              "plan_name",
+              "branch_limit",
+              "user_limit_per_branch",
+              "use_phone_for_sale",
+            ],
+          },
+          {
+            model: Address,
+            attributes: [
+              "id",
+              "cep",
+              "street",
+              "complement",
+              "district",
+              "city",
+              "uf",
+            ],
+          },
+        ],
       });
 
       res.send(company);
@@ -67,29 +108,50 @@ module.exports = {
         social_reason,
         place_number,
         companie_password,
-        cep,
         state,
         nature_of_the_business,
         commercial_email,
         plan_id,
+        address,
       } = req.body;
 
-      const plan = await Plan.findByPk(plan_id);
-
+      const plan = await Plan.findByPk(plan_id, {
+        attributes: [
+          "id",
+          "plan_name",
+          "branch_limit",
+          "user_limit_per_branch",
+          "use_phone_for_sale",
+        ],
+      });
       if (!plan)
         return res.status(404).send({ error: "Plano requisitado não existe" });
 
+      let companyAddress;
+
+      const addressFind = await Address.findOne({
+        where: {
+          cep: address.cep,
+        },
+      });
+
+      if (addressFind) companyAddress = addressFind;
+      else {
+        const newAddress = await Address.create(address);
+
+        companyAddress = newAddress;
+      }
       const company = await Company.create({
         cnpj,
         fantasy_name,
         social_reason,
         place_number,
         companie_password: bcryptjs.hashSync(companie_password),
-        cep,
         state,
         nature_of_the_business,
         commercial_email,
         plan_id,
+        address_id: companyAddress.id,
       });
 
       await company.addPermission(1);
@@ -100,11 +162,19 @@ module.exports = {
         fantasy_name,
         social_reason,
         place_number,
-        cep,
         state,
         nature_of_the_business,
         commercial_email,
         plan,
+        address: {
+          id: companyAddress.id,
+          cep: companyAddress.cep,
+          street: companyAddress.street,
+          complement: companyAddress.complement,
+          district: companyAddress.district,
+          city: companyAddress.city,
+          uf: companyAddress.uf,
+        },
       });
     } catch (error) {
       console.error(error);
@@ -122,7 +192,6 @@ module.exports = {
         social_reason,
         place_number,
         companie_password,
-        cep,
         state,
         nature_of_the_business,
         commercial_email,
@@ -135,7 +204,6 @@ module.exports = {
           "fantasy_name",
           "social_reason",
           "place_number",
-          "cep",
           "state",
           "nature_of_the_business",
           "commercial_email",
@@ -161,7 +229,6 @@ module.exports = {
         company.companie_password = cryptPassword;
       }
 
-      if (cep) company.cep = cep;
       if (state) company.state = state;
       if (nature_of_the_business)
         company.nature_of_the_business = nature_of_the_business;
@@ -189,20 +256,7 @@ module.exports = {
     try {
       const { id } = req.params;
 
-      const company = await Company.findByPk(id, {
-        attributes: [
-          "id",
-          "cnpj",
-          "fantasy_name",
-          "social_reason",
-          "place_number",
-          "cep",
-          "state",
-          "nature_of_the_business",
-          "commercial_email",
-          "plan_id",
-        ],
-      });
+      const company = await Company.findByPk(id);
 
       if (!company)
         return res
