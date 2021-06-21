61ef5c93f48c4e880f9fdfdacfa2d2d249e0f892
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-06-21 15:29:20 -0300

    atualizando inclusões de filial de compra

--- src/controllers/purchase.js
+++ src/controllers/purchase.js
@@ -3,6 +3,7 @@ const PaymentMethod = require("../models/PaymentMethod");
 const ItemPurchase = require("../models/ItemPurchase");
 const Branch = require("../models/Branch");
 const Product = require("../models/Product");
+const Address = require("../models/address");
 
 module.exports = {
   async index(req, res) {
@@ -30,13 +31,19 @@ module.exports = {
             },
             {
               model: Branch,
-              attributes: [
-                "id",
-                "branch_name",
-                "cep",
-                "branch_email",
-                "place_number",
-              ],
+              attributes: ["id", "branch_name", "branch_email", "place_number"],
+              include: {
+                model: Address,
+                attributes: [
+                  "id",
+                  "cep",
+                  "street",
+                  "complement",
+                  "district",
+                  "city",
+                  "uf",
+                ],
+              },
             },
           ],
         });
@@ -56,13 +63,19 @@ module.exports = {
             },
             {
               model: Branch,
-              attributes: [
-                "id",
-                "branch_name",
-                "cep",
-                "branch_email",
-                "place_number",
-              ],
+              attributes: ["id", "branch_name", "branch_email", "place_number"],
+              include: {
+                model: Address,
+                attributes: [
+                  "id",
+                  "cep",
+                  "street",
+                  "complement",
+                  "district",
+                  "city",
+                  "uf",
+                ],
+              },
             },
           ],
         });
@@ -93,13 +106,19 @@ module.exports = {
           },
           {
             model: Branch,
-            attributes: [
-              "id",
-              "branch_name",
-              "cep",
-              "branch_email",
-              "place_number",
-            ],
+            attributes: ["id", "branch_name", "branch_email", "place_number"],
+            include: {
+              model: Address,
+              attributes: [
+                "id",
+                "cep",
+                "street",
+                "complement",
+                "district",
+                "city",
+                "uf",
+              ],
+            },
           },
         ],
       });
