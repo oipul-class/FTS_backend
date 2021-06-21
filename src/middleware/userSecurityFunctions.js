ac4ba3fc74deb0dd9c3b1c547936f2c4b6c2e119
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-06-21 15:18:20 -0300

    adicionando função de segurança na criação de usuario

--- src/middleware/userSecurityFunctions.js
+++ src/middleware/userSecurityFunctions.js
@@ -1,10 +1,88 @@
 const jwt = require("jsonwebtoken");
-const { Op } = require("sequelize");
 const auth = require("../config/auth");
 const Permission = require("../models/Permission");
 const User = require("../models/User");
+const Screen = require("../models/Screen");
+const Branch = require("../models/Branch");
+const Company = require("../models/Company");
 
 module.exports = {
+  userStoreCheck: async (req, res, next) => {
+    try {
+      const token = req.headers.authorization;
+      const [Bearer, retriviedToken] = token.split(" ");
+
+      const payload = jwt.verify(retriviedToken, auth.secret);
+
+      const { branch_id } = req.body;
+
+      if (payload.id && payload.cnpj) {
+        const companyFromPayload = await Company.findOne({
+          where: {
+            id: payload.id,
+            cnpj: payload.cnpj,
+          },
+          include: {
+            model: Branch,
+            required: true,
+            attributes: ["id"],
+            where: {
+              id: branch_id,
+            },
+          },
+        });
+
+        if (!companyFromPayload)
+          return res.status(400).send({
+            error:
+              "Usuário não pode ser criado por que foi enviado um filial que não pertence a companhia logada",
+          });
+      } else if (payload.id && payload.user_cpf) {
+        const userAdmin = await User.findOne({
+          where: {
+            id: payload.id,
+          },
+          include: [
+            {
+              model: Permission,
+              include: {
+                model: Screen,
+                require: true,
+                where: {
+                  id: 1,
+                },
+              },
+            },
+            {
+              model: Branch,
+              required: true,
+              attributes: ["id"],
+              where: {
+                id: branch_id,
+              },
+            },
+          ],
+        });
+
+        if (!userAdmin.Permission)
+          return res.status(400).send({
+            error: "Usuario logado não tem permissão para criar um usuário novo",
+          });
+
+        if (!userAdmin.Branch)
+          return res.status(400).send({
+            error:
+              "Usuario logado não pertence a filial requesitada para criar um usuario novo",
+          });
+      }
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
   userUpdateCheck: async (req, res, next) => {
     try {
       const token = req.headers.authorization;
@@ -35,9 +113,9 @@ module.exports = {
           },
           include: {
             model: Permission,
-            require: true,
-            where: {
-              permission_name: { [Op.substring]: "Administrador" },
+            include: {
+              model: Screen,
+              require: true,
             },
           },
         });
