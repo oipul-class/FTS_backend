67dfe003a9db3511efc49bc15c9d7ad89531d2c4
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-06-23 15:33:07 -0300

    verificação de comapnhia nas funções de segurança de venda

--- src/middleware/saleSecurityFunctions.js
+++ src/middleware/saleSecurityFunctions.js
@@ -5,6 +5,7 @@ const Permission = require("../models/Permission");
 const User = require("../models/User");
 const Screen = require("../models/Screen");
 const Sale = require("../models/Sale");
+const Company = require("../models/Company");
 
 module.exports = {
   userStoreCheck: async (req, res, next) => {
@@ -16,47 +17,92 @@ module.exports = {
 
       const { branch_id } = req.body;
 
-      const user = await User.findOne({
-        where: {
-          id: payload.id,
-          cpf: payload.user_cpf
-        },
-        include: [
-          {
-            model: Permission,
-            attributes: ["id"],
-            include: {
-              model: Screen,
-              require: true,
-              where: {
-                id: 7,
+      if (payload.user_cpf && payload.user_rg) {
+        const user = await User.findOne({
+          where: {
+            id: payload.id,
+            cpf: payload.user_cpf,
+          },
+          include: [
+            {
+              model: Permission,
+              attributes: ["id"],
+              include: {
+                model: Screen,
+                require: true,
+                where: {
+                  id: 7,
+                },
+                attributes: ["id"],
               },
-              attributes: ["id"]
             },
-          },
-          {
-            model: Branch,
-            required: true,
-            attributes: ["id"],
-            where: {
-              id: branch_id,
+            {
+              model: Branch,
+              required: true,
+              attributes: ["id"],
+              where: {
+                id: branch_id,
+              },
             },
-          },
-        ],
-      });
-
-      if (!user.Permissions[0])
-        return res.status(400).send({
-          error: "Usuario logado não tem permissão para criar uma venda",
+          ],
         });
 
-      if (!user.Branch)
-        return res.status(400).send({
-          error:
-            "Usuario logado não pertence a filial requesitada para criar uma venda",
+        if (!user.Permissions[0])
+          return res.status(400).send({
+            error: "Usuario logado não tem permissão para criar uma venda",
+          });
+
+        if (!user.Branch)
+          return res.status(400).send({
+            error:
+              "Usuario logado não pertence a filial requesitada para criar uma venda",
+          });
+
+        return next();
+      } else if (payload.cnpj) {
+        const user = await Company.findOne({
+          where: {
+            id: payload.id,
+            cnpj: payload.cnpj,
+          },
+          include: [
+            {
+              model: Permission,
+              attributes: ["id"],
+              include: {
+                model: Screen,
+                require: true,
+                where: {
+                  id: 7,
+                },
+                attributes: ["id"],
+              },
+            },
+            {
+              model: Branch,
+              required: true,
+              attributes: ["id"],
+              where: {
+                id: branch_id,
+              },
+            },
+          ],
         });
 
-      next();
+        if (!user.Permissions[0] || !user.Permissions[0].Screens)
+          return res.status(400).send({
+            error: "Companhia logada não tem permissão para criar uma venda",
+          });
+
+        if (!user.Branch)
+          return res.status(400).send({
+            error:
+              "Filial requesitada da venda não pertence a companhia logada",
+          });
+
+        return next();
+      }
+      return res.status(404).send({ error: "Dados para verificação faltando" });
     } catch (error) {
       console.error(error);
       res.status(500).send(error);
@@ -71,55 +117,100 @@ module.exports = {
 
       const { id } = req.params;
 
-      if (!payload.id && !payload.user_cpf)
-        return res
-          .status(400)
-          .send({ error: "Usuário logado não é um funcionario" });
-
-      const user = await User.findOne({
-        where: {
-          id: payload.id,
-          cpf: payload.user_cpf
-        },
-        include: [
-          {
-            model: Permission,
-            attributes: ["id"],
-            include: {
-              model: Screen,
-              require: true,
-              where: {
-                id: 7,
-              },
-              attributes: ["id"]
-            },
+      if (payload.user_rg || payload.user_rg) {
+        const user = await User.findOne({
+          where: {
+            id: payload.id,
+            cpf: payload.user_cpf,
           },
-          {
+          include: [
+            {
+              model: Permission,
+              attributes: ["id"],
+              include: {
+                model: Screen,
+                require: true,
+                where: {
+                  id: 7,
+                },
+                attributes: ["id"],
+              },
+            },
+            {
+              model: Branch,
+              attributes: ["id"],
+            },
+          ],
+        });
+
+        const sale = await Sale.findByPk(id, {
+          include: {
             model: Branch,
             attributes: ["id"],
           },
-        ],
-      });
-
-      const sale = await Sale.findByPk(id, {
-        include: {
-          model: Branch,
-          attributes: ["id"],
-        },
-      });
-
-      if (!user.Permissions)
-        return res.status(400).send({
-          error: "Usuario logado não tem permissão para alterar os dados da venda",
         });
 
-      if (user.Branch.id !== purchase.Branch.id)
-        return res.status(400).send({
-          error:
-            "Usuario logado não pertence a filial requesitada para alterar os dados da venda",
+        if (!user.Permissions)
+          return res.status(400).send({
+            error:
+              "Usuario logado não tem permissão para alterar os dados da venda",
+          });
+
+        if (user.Branch.id !== sale.Branch.id)
+          return res.status(400).send({
+            error:
+              "Usuario logado não pertence a filial requesitada para alterar os dados da venda",
+          });
+
+        return next();
+      } else if (payload.cnpj) {
+        const user = await Company.findOne({
+          where: {
+            id: payload.id,
+            cnpj: payload.cnpj,
+          },
+          include: [
+            {
+              model: Permission,
+              attributes: ["id"],
+              include: {
+                model: Screen,
+                require: true,
+                where: {
+                  id: 7,
+                },
+                attributes: ["id"],
+              },
+            },
+            {
+              model: Branch,
+              attributes: ["id"],
+            },
+          ],
         });
 
-      next();
+        const sale = await Sale.findByPk(id, {
+          include: {
+            model: Branch,
+            attributes: ["id"],
+          },
+        });
+
+        if (!user.Permissions)
+          return res.status(400).send({
+            error:
+              "Companhia logada não tem permissão para alterar os dados da venda",
+          });
+
+        if (user.Branch.id !== sale.Branch.id)
+          return res.status(400).send({
+            error: "Filial da venda não pertence a companhia logada",
+          });
+
+        return next();
+      }
+
+      return res.status(404).send({ error: "Dados para verificação faltando" });
     } catch (error) {
       console.error(error);
       res.status(500).send(error);
