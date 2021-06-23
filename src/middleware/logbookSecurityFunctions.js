93945bcb25ac454884a4fd4dce9e3fb7b66c8c91
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-06-23 16:41:17 -0300

    permitindo companhia inserir ou atualizar um produto no inventario da filial

--- src/middleware/logbookSecurityFunctions.js
+++ src/middleware/logbookSecurityFunctions.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
-const { Op } = require("sequelize");
 const auth = require("../config/auth");
 const Branch = require("../models/Branch");
 const Company = require("../models/Company");
 const Permission = require("../models/Permission");
 const Product = require("../models/Product");
+const Logbook = require("../models/LogBookInventory");
 const User = require("../models/User");
 const Screen = require("../models/Screen");
 
@@ -16,68 +16,134 @@ module.exports = {
 
       const payload = jwt.verify(retriviedToken, auth.secret);
 
-      const user = await User.findOne({
-        where: {
-          id: payload.id,
-        },
-        include: [
-          {
-            model: Permission,
+      if (payload.user_cpf && payload.user_rg) {
+        const user = await User.findOne({
+          where: {
+            id: payload.id,
+            cpf: payload.user_cpf,
+          },
+          include: [
+            {
+              model: Permission,
+              include: {
+                model: Screen,
+                require: true,
+                where: {
+                  id: 3,
+                },
+              },
+            },
+            {
+              model: Branch,
+              attributes: ["id"],
+              include: {
+                model: Company,
+                attributes: ["id"],
+              },
+            },
+          ],
+        });
+
+        if (!user.Permissions[0])
+          return res.status(400).send({
+            error: "Usuário não é permitido criar item no inventario da filial",
+          });
+
+        const { product_id, branch_id } = req.body;
+
+        if (user.Branch.id != branch_id)
+          return res.status(400).send({
+            error:
+              "Usuário não trabalha na filial requesitada para criar um item no inventario",
+          });
+
+        const product = await Product.findOne({
+          where: {
+            id: product_id,
+          },
+          include: {
+            model: Company,
+            require: true,
             include: {
-              model: Screen,
+              model: Branch,
               require: true,
               where: {
-                id: 3,
+                id: user.Branch.id,
               },
             },
           },
-          {
-            model: Branch,
-            attributes: ["id"],
-            include: {
-              model: Company,
-              attributes: ["id"],
-            },
+        });
+
+        if (!product)
+          return res.status(400).send({
+            error: "Produto requesitado não existe ou não pertence a companhia",
+          });
+
+        return next();
+      } else if (payload.cnpj) {
+        const { product_id, branch_id } = req.body;
+
+        const product = await Product.findOne({
+          where: {
+            id: product_id,
           },
-        ],
-      });
-
-      if (!user.Permissions[0])
-        return res.status(400).send({
-          error: "Usuário não é permitido criar item no inventario da filial",
-        });
-
-      const { product_id, branch_id } = req.body;
-
-      if (user.Branch.id != branch_id)
-        return res.status(400).send({
-          error:
-            "Usuário não trabalha na filial requesitada para criar um item no inventario",
-        });
-
-      const product = await Product.findOne({
-        where: {
-          id: product_id,
-        },
-        include: {
-          model: Company,
-          require: true,
           include: {
-            model: Branch,
+            model: Company,
             require: true,
-            where: {
-              id: user.Branch.id,
+            include: {
+              model: Branch,
+              require: true,
+              where: {
+                id: branch_id,
+              },
             },
           },
-        },
-      });
-
-      if (!product)
-        return res.status(400).send({
-          error: "Produto requesitado não existe ou não pertence a companhia",
         });
 
-      next();
+        if (!product)
+          return res.status(404).send({ error: "Produto não existe" });
+
+        const user = await Company.findOne({
+          where: {
+            id: payload.id,
+            cnpj: payload.cnpj,
+          },
+          include: [
+            {
+              model: Permission,
+              include: {
+                model: Screen,
+                require: true,
+                where: {
+                  id: 3,
+                },
+              },
+            },
+            {
+              model: Branch,
+              attributes: ["id"],
+              required: true,
+              where: {
+                id: branch_id,
+              },
+            },
+          ],
+        });
+
+        if (!user.Permissions[0] || !user.Permissions[0].Screens[0])
+          return res.status(400).send({
+            error: "Usuário não é permitido criar item no inventario da filial",
+          });
+
+        if (!user.Branches[0])
+          return res.status(404).send({
+            error: "Filial requesitada não pertence a companhia logada",
+          });
+
+        return next();
+      }
+
+      return res.status(404).send({ error: "Dados para verificação faltando" });
     } catch (error) {
       console.error(error);
       res.status(500).send(error);
@@ -91,63 +157,113 @@ module.exports = {
 
       const payload = jwt.verify(retriviedToken, auth.secret);
 
-      if (!payload.user_cpf || !payload.user_rg)
-        return res
-          .status(400)
-          .send({ error: "Usuário logado não é um funcionario" });
-
-      const user = await User.findOne({
-        where: {
-          id: payload.id,
-        },
-        include: [
-          {
-            model: Permission,
-            include: {
-              model: Screen,
-              require: true,
-              where: {
-                id: 3,
+      if (payload.user_cpf && payload.user_rg) {
+        const user = await User.findOne({
+          where: {
+            id: payload.id,
+            cpf: payload.user_cpf
+          },
+          include: [
+            {
+              model: Permission,
+              include: {
+                model: Screen,
+                require: true,
+                where: {
+                  id: 3,
+                },
               },
             },
-          },
-          {
+            {
+              model: Branch,
+              attributes: ["id"],
+              include: {
+                model: Company,
+                attributes: ["id"],
+              },
+            },
+          ],
+        });
+
+        if (!user)
+          return res.status(400).send({
+            error: "Usuário não é permitido criar item no inventario da filial",
+          });
+
+        const { id } = req.params;
+
+        const logbook = await Logbook.findByPk(id, {
+          attributes: ["id"],
+          include: {
             model: Branch,
             attributes: ["id"],
-            include: {
-              model: Company,
-              attributes: ["id"],
-            },
           },
-        ],
-      });
-
-      if (!user)
-        return res.status(400).send({
-          error: "Usuário não é permitido criar item no inventario da filial",
         });
 
-      const { id } = req.params;
+        if (!product)
+          return res.status(404).send({
+            error: "Produto requesitado não existe ou não pertence a companhia",
+          });
 
-      const product = await Product.findByPk(id, {
-        attributes: ["id"],
-        include: {
-          model: Company,
+        if (!logbook.Branch.id != user.Branch.id)
+          return res.status(400).send({
+            error:
+              "Produto requesitado não pertence a companhia dona da filial que o usuário logado trabalha",
+          });
+        return next();
+      } else if (payload.cnpj) {
+        const { id } = req.params;
+
+        const logbook = await Logbook.findByPk(id, {
           attributes: ["id"],
-        },
-      });
-
-      if (!product)
-        return res.status(404).send({
-          error: "Produto requesitado não existe ou não pertence a companhia",
+          include: {
+            model: Branch,
+            attributes: ["id"],
+          },
         });
 
-      if (!product.Company.id != user.Branch.Company.id)
-        return res.status(400).send({
-          error:
-            "Produto requesitado não pertence a companhia dona da filial que o usuário logado trabalha",
+        if (!logbook) return res.status(404).send({ error: "Item no inventario não existe"})
+
+        const user = await User.findOne({
+          where: {
+            id: payload.id,
+            cnpj: payload.cnpj
+          },
+          include: [
+            {
+              model: Permission,
+              include: {
+                model: Screen,
+                require: true,
+                where: {
+                  id: 3,
+                },
+              },
+            },
+            {
+              model: Branch,
+              attributes: ["id"],
+              required: true,
+              where: {
+                id: logbook.Branch.id
+              }
+            },
+          ],
         });
-      next();
+
+        if (!user.Permissions[0] || !user.Permissions[0].Screens[0])
+          return res.status(400).send({
+            error: "Comapnhia não tem permissão para cadastrar um produto no inventario",
+          });
+
+        if (!user.Branch)
+          return res.status(400).send({
+            error:
+              "Produto requesitado não pertence a companhia dona da filial que o usuário logado trabalha",
+          });
+        return next();
+      }
+      return res.status(404).send({ error: "Dados para verificação faltando" });
     } catch (error) {
       console.error(error);
       res.status(500).send(error);
