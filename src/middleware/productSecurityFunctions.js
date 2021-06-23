16033726350428041d58e19e8774a70c49e3e2e3
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-06-23 15:03:15 -0300

    modificando para permitir que uma companhia possa cadastrar ou ataulizar dados de um produto

--- src/middleware/productSecurityFunctions.js
+++ src/middleware/productSecurityFunctions.js
@@ -6,7 +6,7 @@ const auth = require("../config/auth");
 const Branch = require("../models/Branch");
 const Company = require("../models/Company");
 const Product = require("../models/Product");
-const Screen = require("../models/Screen")
+const Screen = require("../models/Screen");
 
 module.exports = {
   productStoreCheck: async (req, res, next) => {
@@ -16,50 +16,94 @@ module.exports = {
 
       const payload = jwt.verify(retriviedToken, auth.secret);
 
-      const user = await User.findOne({
-        where: {
-          id: payload.id,
-        },
-        include: [
-          {
-            model: Permission,
-            include: {
-              model: Screen,
-              required: true,
-              where: {
-                id: 2,
+      if (payload.user_cpf || payload.user_rg) {
+        const user = await User.findOne({
+          where: {
+            id: payload.id,
+            cpf: payload.cpf,
+          },
+          include: [
+            {
+              model: Permission,
+              include: {
+                model: Screen,
+                required: true,
+                where: {
+                  id: 2,
+                },
               },
             },
-          },
-          {
-            model: Branch,
-            attributes: ["id"],
-            include: {
-              model: Company,
+            {
+              model: Branch,
               attributes: ["id"],
+              include: {
+                model: Company,
+                attributes: ["id"],
+              },
             },
+          ],
+        });
+
+        if (!user.Permissions[0])
+          return res.status(400).send({
+            error:
+              "Usuário logado não tem permissão de cadastrar produto na companhia",
+          });
+
+        const { company_id } = req.body;
+
+        if (user.Branch.Company.id != company_id)
+          return res.status(400).send({
+            error:
+              "Usuário logado não é funcionario de uma filial da companhia enviada nos dados do produto",
+          });
+
+        return next();
+      } else if (payload.cnpj) {
+        const user = await Company.findOne({
+          where: {
+            id: payload.id,
+            cnpj: payload.cnpj,
           },
-        ],
-      });
-
-      console.log(user);
-      
-
-      if (!user.Permissions[0])
-        return res.status(400).send({
-          error:
-            "Usuário logado não tem permissão de cadastrar produto na companhia",
+          include: [
+            {
+              model: Permission,
+              include: {
+                model: Screen,
+                required: true,
+                where: {
+                  id: 2,
+                },
+              },
+            },
+            {
+              model: Branch,
+              attributes: ["id"],
+              include: {
+                model: Company,
+                attributes: ["id"],
+              },
+            },
+          ],
         });
 
-      const { company_id } = req.body;
+        if (!user.Permissions[0] || user.Permissions[0].Screen)
+          return res.status(400).send({
+            error:
+              "Companhia logada não tem permissão de cadastrar produto na companhia",
+          });
 
-      if (user.Branch.Company.id != company_id)
-        return res.status(400).send({
-          error:
-            "Usuário logado não é funcionario de uma filial da companhia enviada nos dados do produto",
-        });
+        const { company_id } = req.body;
 
-      next();
+        if (user.id != company_id)
+          return res.status(400).send({
+            error: "Filial recebida não é da companhia logada",
+          });
+
+        return next();
+      }
+
+      return res.status(404).send({ error: "Dados para verificação faltando" });
     } catch (error) {
       console.error(error);
       res.status(500).send(error);
@@ -73,67 +117,124 @@ module.exports = {
 
       const payload = jwt.verify(retriviedToken, auth.secret);
 
-      if (!payload.user_cpf || !payload.user_rg)
-        return res
-          .status(400)
-          .send({ error: "Usuário logado não é um funcionario" });
-
-      const user = await User.findOne({
-        where: {
-          id: payload.id,
-        },
-        include: [
-          {
-            model: Permission,
-            include: {
-              model: Screen,
-              required: true,
-              where: {
-                id: 2,
+      if (payload.user_cpf && payload.user_rg) {
+        const user = await User.findOne({
+          where: {
+            id: payload.id,
+            cpf: payload.cpf,
+          },
+          include: [
+            {
+              model: Permission,
+              include: {
+                model: Screen,
+                required: true,
+                where: {
+                  id: 2,
+                },
               },
             },
-          },
-          {
-            model: Branch,
-            attributes: ["id"],
-            include: {
-              model: Company,
+            {
+              model: Branch,
               attributes: ["id"],
+              include: {
+                model: Company,
+                attributes: ["id"],
+              },
             },
+          ],
+        });
+
+        if (!user.Permission)
+          return res.status(400).send({
+            error:
+              "Usuário logado não tem permissão para alterar dados de produtos cadastrados na companhia",
+          });
+
+        const { company_id } = req.body;
+
+        if (user.Branch.Company.id != company_id)
+          return res.status(400).send({
+            error:
+              "Usuário logado não é funcionario de uma filial da companhia enviada nos dados do produto",
+          });
+
+        const { id } = req.params;
+
+        const product = await Product.findOne({
+          where: {
+            id,
+            company_id,
           },
-        ],
-      });
-
-      if (!user.Permission)
-        return res.status(400).send({
-          error:
-            "Usuário logado não tem permissão para alterar dados de produtos cadastrados na companhia",
         });
 
-      const { company_id } = req.body;
+        if (!product)
+          return res.status(400).send({
+            error:
+              "Produto requesitado a ser alterado não pertence ou não esta cadastrado na companhia",
+          });
 
-      if (user.Branch.Company.id != company_id)
-        return res.status(400).send({
-          error:
-            "Usuário logado não é funcionario de uma filial da companhia enviada nos dados do produto",
+        return next();
+      } else if (payload.cnpj) {
+        const user = await Company.findOne({
+          where: {
+            id: payload.id,
+            cnpj: payload.cnpj,
+          },
+          include: [
+            {
+              model: Permission,
+              include: {
+                model: Screen,
+                required: true,
+                where: {
+                  id: 2,
+                },
+              },
+            },
+            {
+              model: Branch,
+              attributes: ["id"],
+              include: {
+                model: Company,
+                attributes: ["id"],
+              },
+            },
+          ],
         });
 
-      const { id } = req.params;
+        if (!user.Permission[0] || !user.Permission[0].Screen)
+          return res.status(400).send({
+            error:
+              "Comapnhia logada não tem permissão para alterar dados de produtos cadastrados",
+          });
 
-      const product = await Product.findOne({
-        where: {
-          id,
-          company_id,
-        },
-      });
+        const { company_id } = req.body;
 
-      if (!product)
-        return res.status(400).send({
-          error:
-            "Produto requesitado a ser alterado não pertence ou não esta cadastrado na companhia",
+        if (user.id != company_id)
+          return res.status(400).send({
+            error: "Filial recebida não é da companhia logada",
+          });
+
+        const { id } = req.params;
+
+        const product = await Product.findOne({
+          where: {
+            id,
+            company_id,
+          },
         });
 
-      next();
+        if (!product)
+          return res.status(400).send({
+            error:
+              "Produto requesitado a ser alterado não pertence ou não esta cadastrado na companhia",
+          });
+
+        return next();
+      }
+
+      return res.status(404).send({ error: "Dados para verificação faltando" });
     } catch (error) {
       console.error(error);
       res.status(500).send(error);
