c6fca36d45fb90f3ef4e008edc92b06598bb3e01
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-06-29 19:13:06 -0300

    começando a stream para upload

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,25 @@
-const admin = require("firebase-admin");
+var admin = require("firebase-admin");
 
-const serviceAccount = require("../config/firebase-key.js");
+var serviceAccount = require("../config/firebase-key.js");
 
-const firebaseBucket = "flow-trading-system.appspot.com"; 
+const firebaseBucket = "flow-trading-system.appspot.com";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket:  firebaseBucket,
+  storageBucket: firebaseBucket,
 });
 
 const uploadImage = (req, res, next) => {
   if (!req.file) return next();
 
-  const image = req.file;
+  const bucketInstance = admin.storage().bucket();
 
-  
-}
+  const image = req.file;
+  const imageName = Date.now() + "." + image.originalName.split(".").pop();
+
+  const imageFile = bucketInstance.file(imageName);
+
+  const stream = imageFile.createWriteStream
+};
+
+module.exports = [uploadImage];
