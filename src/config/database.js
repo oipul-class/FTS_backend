2eaa6288351b057846244d778c63b13cd66cfb62
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-06-29 15:13:48 -0300

    revertendo teste

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.NODE_ENV === "test" ? process.env.TESTE_DATABASE_DB  : process.env.DATABASE_DB ,
+  database: process.env.DATABASE_DB ,
   dialect: "mysql",
   define: {
     timezone: "-2:00",
