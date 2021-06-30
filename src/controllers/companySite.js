8c7420020b9a01a1369982c37c3b340cf44c734c
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-06-30 15:28:24 -0300

    criando controller de site de companhia

--- src/controllers/companySite.js
+++ src/controllers/companySite.js
@@ -1,8 +1,36 @@
+const Company = require("../models/Company");
+const Website = require("../models/Website");
+
 module.exports = {
   async store(req, res) {
     try {
-      return console.log(req.file);
-      ;
+      const { company_id } = req.params;
+      const { website_slogan, website_customization } = req.body;
+      const { firebase_url } = req.file;
+
+      const company = await Company.findByPk(company_id);
+
+      if (!company)
+        return res.status(400).send({
+          error: "Comapnhia não existe",
+        });
+
+      if (company.WebsiteId) return res.status(400).send({ error: "Companhia já possui o site"})
+      if (typeof website_customization == String)
+        return res
+          .status(400)
+          .send({ error: "Configurações não estão no formato requestidao" });
+
+      const website = await company.createWebsite({
+        website_logo: firebase_url,
+        website_customization,
+      });
+
+      res.status(201).send({
+        id: website.id,
+        website_logo: website.website_logo,
+        website_customization: website.website_customization,
+      });
     } catch (error) {
       console.error(error);
       res.status(500).send(error);
