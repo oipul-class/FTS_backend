a72ebc88b8f63e835c621aa2bb15461dc4cb6c84
JOÃO LUCAS SOARES BEZERRA <20185472@CFP127ALUNO.LOCAL>
2021-06-30 15:28:01 -0300

    criando model de site e associando a companhia

---
+++ src/models/Website.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Website extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        website_logo: DataTypes.STRING,
+        website_customization: DataTypes.TEXT,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.hasOne(models.Company)
+  }
+}
+
+module.exports = Website;
