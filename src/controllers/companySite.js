a638c1f2cbfc99ec87c746b077eb970f664964e5
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-07-03 23:22:17 -0300

    adicionando função de busca

--- src/controllers/companySite.js
+++ src/controllers/companySite.js
@@ -1,6 +1,89 @@
+const Address = require("../models/Address");
+const Branch = require("../models/Branch");
 const Company = require("../models/Company");
+const Phone = require("../models/Phone");
+const Product = require("../models/Product");
+const ProductType = require("../models/ProductType");
+const UnitOfMeasurement = require("../models/UnitOfMeasurement");
+const Website = require("../models/Website");
 
 module.exports = {
+  async index(req, res) {
+    try {
+      const { company_id } = req.params;
+
+      const site = await Website.findOne({
+        attributes: [
+          "id",
+          "logo_img",
+          "banner_img",
+          "slogan",
+          "primary_color",
+          "secondary_color",
+          "light_color",
+        ],
+        include: {
+          model: Company,
+          required: true,
+          where: {
+            id: company_id,
+          },
+          attributes: [
+            "id",
+            "cnpj",
+            "fantasy_name",
+            "social_reason",
+            "place_number",
+            "nature_of_the_business",
+            "commercial_email",
+            "plan_id",
+          ],
+          include: [
+            {
+              model: Branch,
+              attributes: ["id", "branch_name", "branch_email", "place_number"],
+              include: [
+                {
+                  model: Phone,
+                  attributes: ["id", "phone"],
+                },
+                {
+                  model: Address,
+                  attributes: [
+                    "cep",
+                    "street",
+                    "complement",
+                    "district",
+                    "city",
+                    "uf",
+                  ],
+                },
+              ],
+            },
+            {
+              model: Product,
+              attributes: [
+                "id",
+                "product_name",
+                "description",
+                "cost_per_item",
+              ],
+              include: {
+                model: ProductType,
+                attributes: ["id", "type"],
+              },
+            },
+          ],
+        },
+      });
+
+      res.send(site);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  },
+
   async store(req, res) {
     try {
       const { company_id } = req.params;
