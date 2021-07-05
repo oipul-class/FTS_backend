ee9fd1524b8d03a9357b37e1565e1d48dd668659
oipul-class <joaolucassoaresbezerra@gmail.com>
2021-07-04 23:41:03 -0300

    revertendo

--- src/app.js
+++ src/app.js
@@ -13,29 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    methods: ["POST"],
-  })
-);
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 app.use(routes);
 app.use(errors());
